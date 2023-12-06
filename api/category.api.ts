02f80ed4d259c60e9824ce60bba67f24e415d7f6
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-06 18:07:43 +0600

    update catgory response type
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- api/category.api.ts
+++ api/category.api.ts
@@ -1,18 +1,33 @@
+import { PaginationAPIResponse } from '~/types/response';
 import { api } from './base';
+
+export interface Category {
+    id: string;
+    name: string;
+    value: string;
+    userId: string;
+    user: {
+        id: string;
+        name: string;
+        type: 'ADMIN' | 'USER';
+    };
+}
 
 export function getAllCategoriesOptions() {
     return api(`/categories/options`, { method: 'get' });
 }
 
 export function getAllCategories(page = 1) {
-    return api(`/categories?page=${page}`, { method: 'get' });
+    return api<PaginationAPIResponse<Category[]>>(`/categories?page=${page}`, {
+        method: 'get',
+    });
 }
 
 export function deleteCategory(id: string) {
     return api(`/categories/${id}`, { method: 'delete' });
 }
 
-export function saveCategory(data: any, id?: string) {
+export function saveCategory(data: { name: string }, id?: string) {
     if (id) {
         return api(`/categories/edit/${id}`, {
             method: 'patch',
