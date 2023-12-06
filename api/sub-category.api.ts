033146e7c079c16a8b1c15359f73cbbc87f639b6
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-06 18:28:58 +0600

    improve subcategory types
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- api/sub-category.api.ts
+++ api/sub-category.api.ts
@@ -1,18 +1,52 @@
+import type { APIResponse, PaginationAPIResponse } from '~/types/response';
 import { api } from './base';
 
+export interface SubCategoryOpt {
+    id: string;
+    name: string;
+    value: string;
+}
+
+export interface SubCategory extends SubCategoryOpt {
+    description: string;
+    userId: string;
+    user: {
+        id: string;
+        name: string;
+        type: string;
+    };
+    categoryId: string;
+    category: {
+        id: string;
+        name: string;
+        value: string;
+    };
+}
+
 export function getAllSubCategoriesOptions(category: string) {
-    return api(`/sub-categories/${category}/options`, { method: 'get' });
+    return api<PaginationAPIResponse<SubCategoryOpt[]>>(
+        `/sub-categories/${category}/options`,
+        { method: 'get' }
+    );
 }
 
 export function getAllSubCategories(category: string, page = 1) {
-    return api(`/sub-categories/${category}?page=${page}`, { method: 'get' });
+    return api<PaginationAPIResponse<SubCategory[]>>(
+        `/sub-categories/${category}?page=${page}`,
+        { method: 'get' }
+    );
 }
 
 export function deleteSubCategory(id: string) {
-    return api(`/sub-categories/${id}`, { method: 'delete' });
+    return api<APIResponse<null>>(`/sub-categories/${id}`, {
+        method: 'delete',
+    });
 }
 
-export function saveSubCategory(data: any, id?: string) {
+export function saveSubCategory(
+    data: { name: string; description: string },
+    id?: string
+) {
     if (id) {
         return api(`/sub-categories/edit/${id}`, {
             method: 'patch',
