d9fc16f62bfa37c5a908865c6fdcd8b2cc27eb6c
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-12-06 18:23:25 +0600

    increase password char limit
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -14,7 +14,7 @@ definePageMeta({
 const formSchema = toTypedSchema(
     z.object({
         email: z.string().email('invalid email'),
-        password: z.string().min(5).max(10),
+        password: z.string().min(5).max(20),
     })
 );
 
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -15,7 +15,7 @@ const formSchema = toTypedSchema(
     z.object({
         name: z.string(),
         email: z.string().email('invalid email'),
-        password: z.string().min(5).max(10),
+        password: z.string().min(5).max(20),
     })
 );
 
